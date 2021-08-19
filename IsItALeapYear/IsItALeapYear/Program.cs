eb3e74ef3e25a40324266ca84a02ede71b604b30
Vaidehi <Vdarji@confederationcollege.ca>
2021-08-18 22:18:47 -0400

    Final running Lab of IsItALeapYear

---
+++ IsItALeapYear/IsItALeapYear/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+            public bool IsItALeapYear(int year)
+            {
+                if ((year % 4 == 0) && (year % 100 != 0))
+                {
+                    return true;
+                }
+                if (year % 400 == 0)
+                {
+                    return true;
+                }
+
+                else
+                {
+                    return false;
+                }
+            }   
+    }
+}
---
+++ IsItALeapYear/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19a211c0-9196-4bbc-b4b0-3a06be830343")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
